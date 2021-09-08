1092196bd600b714ddcf4cf755187da60196577c
enkanga <enkanga@gmail.com>
2021-09-07 23:43:06 -0400

    Initial addition of first two scripts of Unit 2 section 4

---
+++ Unit2_SimplePrograms/4_Functions/square.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 22:01:08 2021
+
+@author: ENkanga
+"""
+
+'''
+Write a Python function, square, that takes in one number and returns the square of that number.
+
+This function takes in one number and returns one number.
+'''
+
+def square(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    return x ** 2
